ed6a6a1a47ddbb5e5971e3f5c54e218f1b415cd4
Lunathor <mrcoopgmz@gmail.com>
2025-01-19 16:10:02 +0300

    Initial commit

---
+++ ChatikNew/accounts/views.py
@@ -0,0 +1,30 @@
+from django.db import IntegrityError
+from django.views.generic import ListView
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from chat.models import *
+# Create your views here.
+
+class Profile(ListView):
+    model = User
+    template_name = 'profile.html'
+    
+    def Logout(request):
+        logout(request)
+        redirect('')
+        
+    
+    def signup(request):
+        if request.method == 'POST':
+            username = request.POST['username']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+            
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username=username, password=password1, photo='gallery/images/defaultphoto.jpg')
+            except IntegrityError:
+                msg = 'Такой пользователь уже существует'
+                return render(request, 'registration/signup.html', {'msg' : msg})
+        
+        return render(request, 'registration/signup.html')
---
+++ ChatikNew/chat/models.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import ForeignKey
+# Create your models here.
+
+class User(AbstractUser):
+    photo = models.ImageField(upload_to='gallery/images/', null=True, blank=True)
+    
+    
+class Chat(models.Model):
+    title = models.CharField(max_length=20)
+    users = models.ManyToManyField(User, related_name='chats')
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Message(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, related_name='messages', on_delete=models.CASCADE)
+    text = models.TextField()
+    sendtime = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ('sendtime',)
+        
+    
